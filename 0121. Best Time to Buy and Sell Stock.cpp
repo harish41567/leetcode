ef8669b1c67a324e77593e8bc7c78f4a2912a63b
Harish <75532358+harish41567@users.noreply.github.com>
2022-02-05 17:37:46 +0530

    0121

--- 0121. Best Time to Buy and Sell Stock.cpp
+++ 0121. Best Time to Buy and Sell Stock.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        
-        int res=0;
-        int prev=0; // buy and sell on the first day
-        
-        
-        for(int i=1;i<n;i++){
-           int curr=max(prev+(prices[i]-prices[i-1]), 0); // either extend previous profit or start from the current day
-            prev=curr;
-            res=max(res,curr);
+    int maxProfit(vector<int>& prices) 
+    {
+        int n = prices.size();
+
+        int res = 0;
+        int prev = 0; // buy and sell on the first day
+
+
+        for (int i = 1; i < n; i++) 
+        {
+            int curr = max(prev + (prices[i] - prices[i - 1]), 0); // either extend previous profit or start from the current day
+            prev = curr;
+            res = max(res, curr);
         }
-        
+
         return res;
-        
-        
-        
-        
-        
+
 //         int n=prices.size();
 //         vector<int> dp(n);
-        
+
 //         int res=0;
 //         dp[0]=0; // buy and sell on the first day
-        
+
 //         // dp[i] is the profit if we sell the stock on the ith day
 //         for(int i=1;i<n;i++){
 //             dp[i]=max(dp[i-1]+(prices[i]-prices[i-1]), 0); // either extend previous profit or start from the current day
 //             res=max(res,dp[i]);
 //         }
-        
+
 //         return res;
     }
 };
