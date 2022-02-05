6da87e50ca914b084551a63870e82ecb2ddfd2a5
Harish <75532358+harish41567@users.noreply.github.com>
2022-02-05 17:35:26 +0530

    0112

--- 0112. Path Sum.cpp
+++ 0112. Path Sum.cpp
@@ -12,13 +12,13 @@
 class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
-        
-        if(root==NULL)
+
+        if (root == NULL)
             return false;
-        
-        if(root->val == targetSum && !root->left && !root->right)
+
+        if (root->val == targetSum && !root->left && !root->right)
             return true;
-        
-        return hasPathSum(root->left, targetSum-root->val) || hasPathSum(root->right, targetSum-root->val);
+
+        return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
     }
 };
