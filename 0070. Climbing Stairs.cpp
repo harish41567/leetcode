5cade068c1458644a79e48cbc921499c54556b98
Harish <harish46715@gmail.com>
2022-02-14 17:12:07 +0530

    0070

---
+++ 0070. Climbing Stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1)
+            return 1;
+
+        int first = 1;
+        int second = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int temp = second;
+            second = first + second;
+            first = temp;
+        }
+
+        return second;
+    }
+};
