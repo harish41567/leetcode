6b5e6edf231b612a76225fdd19ab11e35bb0fd99
Harish <75532358+harish41567@users.noreply.github.com>
2022-02-17 21:08:07 +0530

    0977

--- 0977. Squares of a Sorted Array.cpp
+++ 0977. Squares of a Sorted Array.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int n=nums.size();
-        int l=0,r=n-1,i=n-1;
+        int n = nums.size();
+        int l = 0, r = n - 1, i = n - 1;
         vector<int> v(n);
-        
-        while(l<=r){
-            if(abs(nums[l]) <= abs(nums[r]))
+
+        while (l <= r) {
+            if (abs(nums[l]) <= abs(nums[r]))
             {
-                v[i--]=nums[r]*nums[r];
+                v[i--] = nums[r] * nums[r];
                 r--;
             }
             else
             {
-                v[i--]=nums[l]*nums[l];
+                v[i--] = nums[l] * nums[l];
                 l++;
             }
         }
