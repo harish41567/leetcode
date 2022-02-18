982ec69666d5f29e747bdaad72400ee732dbf76c
Harish <75532358+harish41567@users.noreply.github.com>
2022-02-18 22:30:49 +0530

    1358

--- 1358. Number of Substrings Containing All Three Characters.cpp
+++ 1358. Number of Substrings Containing All Three Characters.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    int numberOfSubstrings(string s) {
-        int n=s.length();
-        
-        vector<int> c={0,0,0};
-        int i=0,j=0,res=0;
-        
-        for(int j=0;j<n;j++){
-            c[s[j]-'a']++;
-            
-            while(c[0] && c[1] && c[2]){
-                
-                c[s[i]-'a']--;
+    int numberOfSubstrings(string s)
+    {
+        int n = s.length();
+        vector<int> c = {0, 0, 0};
+        int i = 0, j = 0, res = 0;
+
+        for (int j = 0; j < n; j++)
+        {
+            c[s[j] - 'a']++;
+
+            while (c[0] && c[1] && c[2])
+            {
+                c[s[i] - 'a']--;
                 i++;
             }
-            
-            res+=i;
+
+            res += i;
         }
+
         return res;
     }
 };
