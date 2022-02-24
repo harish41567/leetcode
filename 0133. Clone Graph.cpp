3c99918803dbee71fae714fe90d66ef3fcf3a959
Harish <harish46715@gmail.com>
2022-02-24 22:19:11 +0530

    0133

---
+++ 0133. Clone Graph.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+private:
+    unordered_map<Node*, Node*> map;
+
+    Node* dfs(Node* node)
+    {
+        if (map.find(node) != map.end())
+            return map[node];
+
+        Node* clone = new Node(node->val);
+        map[node] = clone;
+
+        for (Node* n : node->neighbors)
+            clone->neighbors.push_back(dfs(n));
+
+        return clone;
+    }
+
+public:
+    Node* cloneGraph(Node* node) {
+        if (node == NULL)
+            return NULL;
+        return dfs(node);
+    }
+};
