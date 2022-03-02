07b67699809e766f3a7ceab3c1b5f9b930fc5c03
Harish <75532358+harish41567@users.noreply.github.com>
2022-03-02 21:13:23 +0530

    0207

---
+++ 0207. Course Schedule.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        vector<int> indegree(n, 0);
+        vector<int> adj[n];
+        for (auto it : prerequisites)
+        {
+            adj[it[1]].push_back(it[0]);
+            indegree[it[0]]++;
+        }
+
+        int cnt = 0;
+        queue<int> q;
+        for (int i = 0; i < n; i++)
+        {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            cnt++;
+            for (auto it : adj[node])
+            {
+                indegree[it]--;
+
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+
+        if (cnt == n)
+            return true;
+
+        return false;
+    }
+};
