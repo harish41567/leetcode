72a65db927b15c78cc1dcf8bb0760b19e820f7da
Harish <75532358+harish41567@users.noreply.github.com>
2022-03-09 19:48:26 +0530

    0258

---
+++ 0258. Add Digits.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    int SumOfDigits(int num)
+    {
+        int sum = 0;
+        while (num)
+        {
+            sum += (num % 10);
+            num /= 10;
+        }
+
+        return sum;
+    }
+
+public:
+    int addDigits(int num) {
+        while (num > 9)
+            num = SumOfDigits(num);
+
+        return num;
+    }
+};
