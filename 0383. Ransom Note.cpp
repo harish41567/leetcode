d39f7302fde88cb35df20047f44511dac15382dc
Harish <75532358+harish41567@users.noreply.github.com>
2022-03-15 22:41:24 +0530

    0383

---
+++ 0383. Ransom Note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) 
+    {
+        int n = magazine.length();
+        vector<int> mp(26, 0);
+        for(char c : magazine)
+            mp[c - 'a']++;
+        
+        for(char c : ransomNote)
+        {
+            mp[c - 'a']--;
+            
+            if(mp[c - 'a'] < 0)
+                return false;  
+        }
+        return true;
+    }
+};
