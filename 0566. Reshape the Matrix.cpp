2a79e0ed59a68ed8b8d339007a57ad2f8a4feca2
Harish <75532358+harish41567@users.noreply.github.com>
2022-03-28 22:39:43 +0530

    0566

---
+++ 0566. Reshape the Matrix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c)
+    {
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> ans(r, vector<int> (c, 0));
+        if (m * n != r * c)
+            return mat;
+
+        for (int i = 0; i < r * c; i++)
+            ans[i / c][i % c] = mat[i / n][i % n];
+
+        return ans;
+    }
+};
